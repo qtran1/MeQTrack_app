92ebe659bec408ef726302c794752ae6cffa1fe5
Quynh Tran <qtran@stjude.org>
2026-05-21 22:56:12 -0500

    v2.1.0: bump MEQTRACK_VERSION to 2.1.0

--- pipeline/methylation_pipeline.R
+++ pipeline/methylation_pipeline.R
@@ -22,7 +22,7 @@ rm(list=ls())
 # Process command line arguments
 suppressPackageStartupMessages(library(optparse))
 
-MEQTRACK_VERSION <- "2.0.0"
+MEQTRACK_VERSION <- "2.1.0"
 
 cat("┌───────────────────────────────────────────────────┐\n")
 cat("│                     MeQTrack                      │\n")
